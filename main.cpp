b6938fc4d41d39fdf3379b6a7d333b96b4b8108e
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2023-04-28 13:57:27 +0900

    Add new module sample

--- main.cpp
+++ main.cpp
@@ -13,7 +13,21 @@ int main() {
   vm->exec("a = [1, 2, 3]", "main.py", EXEC_MODE);
 
   // Eval the sum of the list
-  PyObject *result = vm->exec("sum(a)", "<eval>", EVAL_MODE);
+  PyObject *result = vm->exec("sum(a)", "main.py", EVAL_MODE);
   std::cout << CAST(int, result) << std::endl; // 6
+
+  // Create a new Python module
+  PyObject *mymodule = vm->new_module("mymodule");
+  // Add a function to mymodule
+  vm->bind_func<2>(mymodule, "add", [](VM *vm, ArgsView args) {
+    i64 lhs = CAST(i64, args[0]);
+    i64 rhs = CAST(i64, args[1]);
+    return VAR(lhs + rhs);
+  });
+  vm->exec("import mymodule", "main.py", EXEC_MODE);
+  PyObject *obj = vm->exec("mymodule.add(10, 20)", "main.py", EVAL_MODE);
+  int i = CAST(int, obj);
+  std::cout << i << std::endl;
+
   return 0;
 }
